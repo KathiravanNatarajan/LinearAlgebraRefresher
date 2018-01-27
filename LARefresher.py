d8f6a5d81762196c1df84c4b1e64ebe1474062a6
KathiravanNatarajan <sunkathirav@gmail.com>
2018-01-27 16:17:40 -0600

    Decimal type is used for Vectors to preserve the precision

--- LARefresher.py
+++ LARefresher.py
@@ -2,16 +2,19 @@
 """
 Created on Sun Jan 21 10:39:02 2018
 
-@author: sunka
+@author: Kathiravan Natarajan
 """
 import math
-
+from decimal import Decimal, getcontext 
+getcontext().prec = 30 
 class Vector(object):
     def __init__(self, coordinates):
+        
+        CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -41,7 +44,7 @@ class Vector(object):
         return Vector(subtracted)
         
     def scalar_multiply(self,s):
-        multiplied = [s*x for x in self.coordinates]
+        multiplied = [Decimal(s)*x for x in self.coordinates]
         return Vector(multiplied)
         
     def magnitude(self):
@@ -55,23 +58,29 @@ class Vector(object):
             unit_vector = self.scalar_multiply(1/mag)
             return unit_vector
         except ZeroDivisionError:
-            raise Exception("Cannot normalize the zero vector")
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
     def dot_product(v1,v2):
         dot = sum([x*y for x, y in zip(v1.coordinates,v2.coordinates)])
         return dot     
         
     def angle_of_vectors(v1,v2,second = False):
-        if second == True:
-            x = Vector.dot_product(Vector.normalize(v1),Vector.normalize(v2))
-        else:
-            x = (Vector.dot_product(v1,v2))/(v1.magnitude() * v2.magnitude())       
-        angle_in_rad = math.acos(x)
-        angle_in_deg = math.degrees(angle_in_rad)
-        return angle_in_rad, angle_in_deg
+        try:
+            if second == True:
+                x = Vector.dot_product(Vector.normalize(v1),Vector.normalize(v2))
+            else:
+                x = (Vector.dot_product(v1,v2))/(v1.magnitude() * v2.magnitude())       
+            angle_in_rad = math.acos(x)
+            angle_in_deg = math.degrees(angle_in_rad)
+            return angle_in_rad, angle_in_deg
+        except Exception as e:
+            if str(e) == Vector.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot compute angle with zero vector') 
+            else:
+                raise e
 
         
-my_vector = Vector([3,4,1])
-my_vector2 = Vector([2,5,1])
+my_vector = Vector([1,2,3])
+my_vector2 = Vector([2,4,6])
 print my_vector == my_vector2
 print my_vector + my_vector2
 print Vector.subtract(my_vector,my_vector2)
@@ -81,4 +90,4 @@ print my_vector.normalize()
 print Vector.dot_product(my_vector,my_vector2)
 m1=Vector([7.35,0.221,5.188])
 m2=Vector([2.751,8.259,3.985])
-print Vector.angle_of_vectors(m1,m2,True)
+print Vector.angle_of_vectors(my_vector,my_vector2,True)
